ff067b46c97955cdb97487789d564feccd357567
sxlvioad <tattotg@gmail.com>
2022-02-13 16:46:10 -0300

    Correccion Error

--- src/components/Item-detail-container/ItemDetailContainer.js
+++ src/components/Item-detail-container/ItemDetailContainer.js
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import useProducts from "../../hooks/useProducts";
 
 const ItemDetailContainer = () => {
-
   const { products } = useProducts();
 
   const { id } = useParams();
@@ -14,34 +13,32 @@ const ItemDetailContainer = () => {
     if (products.length > 0) {
       const selectedProduct = products.find((product) => product.id === id);
       setSelectedItem(selectedProduct);
-      console.log("seleecionado",selectedItem)
+      console.log("seleecionado", selectedItem);
     }
   }, [products]);
 
+  return (
+    <>
+      {selectedItem && (
+        <div className="detailContainer">
+          <div className="probemos">
+            <h1>{selectedItem.name && selectedItem.name}</h1>
 
-    
-      return (
-          <div className='detailContainer'>
-            <div className='probemos'>
-                  <h1>{selectedItem.name && selectedItem.name}</h1>
-                  
-                  <hr/>
-                  {selectedItem.artist && <h2>By: {selectedItem.artist}</h2>}
-                  {selectedItem.genre && <h2>By: {selectedItem.genre}</h2>}
-                  {selectedItem.type && <h2>By: {selectedItem.type}</h2>}
-                  {selectedItem.price && <h2>By: {selectedItem.price}</h2>}
-              </div>
-
-
-              <div>
-                  {selectedItem.img && <img className='imgDetail' src={selectedItem.img} alt='A'/> }
-              
-              </div>
-
-
-
+            <hr />
+            {selectedItem.artist && <h2>By: {selectedItem.artist}</h2>}
+            {selectedItem.genre && <h2>By: {selectedItem.genre}</h2>}
+            {selectedItem.type && <h2>By: {selectedItem.type}</h2>}
+            {selectedItem.price && <h2>By: {selectedItem.price}</h2>}
+          </div>
+          <div>
+            {selectedItem.img && (
+              <img className="imgDetail" src={selectedItem.img} alt="A" />
+            )}
+          </div>
         </div>
-    )
+      )}
+    </>
+  );
 };
 
-export default ItemDetailContainer
+export default ItemDetailContainer;
